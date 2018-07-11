19339d14eeba01ffe084d0349defeabe5116cdcf
Manuel Vázquez Acosta <manuel@merchise.org>
2018-07-11 09:05:21 -0400

    Add the basis for the type-checker algorithm.
    The type-checker algorithm is fully described in chapter 9 (but you should
    also read chapter 8) of 'The Implementation of Functional Programming
    Languages' by Simon L. Peyton Jones and others.
    
    The core of the type-checker algorithm is done with an unification algorithm
    in the terms of find a *substitution* phi that makes two types t1 and t2 the
    same:
    
      to solve                   t1 = t2
    
      we find phi such that:     subtype phi t1 = subtype phi t2
    
    In our first attempts we'll probably implement the algorithm as closely as
    possible to the one in the Book.  But, it should be clear that we should cast
    this type-checking problem in the language of the general term-unification
    algorithm this branch is all about.

---
+++ tests/test_types.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# ---------------------------------------------------------------------
+# Copyright (c) Merchise Autrement [~º/~] and Contributors
+# All rights reserved.
+#
+# This is free software; you can do what the LICENCE file allows you to.
+#
+from functools import partial
+from xoutil.fp.tools import compose
+
+from xopgi.ql.lang.types import TypeVariable as T, FunctionType as F
+from xopgi.ql.lang.types import scompose, subtype, genvars, delta
+
+
+# The id function type
+I = F(T('a'), T('a'))
+
+
+# The K combinator: a -> b -> a
+K = F(T('a'), F(T('b'), T('a')))
+
+
+# The S combinator: Lx Ly Lz. x z (y z)
+# (a -> b -> c) -> (a -> b) -> b -> c
+bc = F(T('b'), T('c'))
+S = F(
+    F(T('a'), bc),
+    F(F(T('a'), T('b')), bc)
+)
+
+
+def test_scompose_property():
+    # subtype (scompose f g) = (subtype f) . (subtype g)
+    f = delta('a', T, 'id')
+    g = delta('b', T, 'bb')
+    fog1 = partial(subtype, scompose(f, g))
+    fog2 = compose(partial(subtype, f), partial(subtype, g))
+    assert fog1(I) == fog2(I), f'{fog1(I)} != {fog2(I)}'
+    assert fog1(K) == fog2(K), f'{fog1(K)} != {fog2(K)}'
+    assert fog1(S) == fog2(S), f'{fog1(S)} != {fog2(S)}'
+
+
+def test_genvars():
+    assert list(genvars(limit=2)) == [T('.a0', check=False), T('.a1', check=False)]
---
+++ xopgi/ql/lang/types/typecheck/__init__.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# ---------------------------------------------------------------------
+# Copyright (c) Merchise Autrement [~º/~] and Contributors
+# All rights reserved.
+#
+# This is free software; you can do what the LICENCE file allows you to.
+#
+'''A naive type-checking algorithm for the enriched lambda calculus.
+
+An experimental Python implementation of the same type-checker implemented in
+chapter 9 of 'The Implementation of Functional Programming Languages'.
+
+'''
+from typing import Mapping, Any
+
+from .. import Type
+
+
+class AST:
+    pass
+
+
+class Variable(AST):
+    def __init__(self, name: str):
+        self.name = name
+
+
+class Literal(AST):
+    # An extension to the algorithm.  Literals are allowed, but have a
+    # definite type: the most specific type possible.
+    def __init__(self, value: Any, type_: Type):
+        self.value = value
+        self.type = type_
+
+
+class LambdaAbs(AST):
+    def __init__(self, varname: str, body: AST):
+        self.varname = varname
+        self.body = body
+
+
+class Application(AST):
+    def __init__(self, e1: AST, e2: AST):
+        self.e1 = e1
+        self.e2 = e2
+
+
+class _LetExpr(AST):
+    def __init__(self, bindings: Mapping[str, AST], body: AST):
+        self.bindings = bindings
+        self.body = body
+
+
+class LetExpression(_LetExpr):
+    pass
+
+
+class LetrecExpression(_LetExpr):
+    pass
