fb4112c8859fe3a88c4538af60238951ee8ec8c7
Manuel Vázquez Acosta <manuel@merchise.org>
2018-08-12 09:56:38 -0400

    Remove the 'lambda' keyword from the parser.
    As said before, having two different ways to represent the same concept is a
    bad idea.
    
    It was even worse because I allowed the arrow to be used in keyword lambdas,
    let's see it at its grandest splendor:
    
       \a -> lambda b -> b
    
    which thanks to the Gods of PLY parses correctly; but... seriously?!
    
    Therefore, the 'lambda' keyword is out.

--- xopgi/ql/lang/expressions/parser.py
+++ xopgi/ql/lang/expressions/parser.py
@@ -57,7 +57,7 @@ tokens = [
 ]
 
 reserved = [
-    'letrec', 'let', 'in', 'lambda'
+    'letrec', 'let', 'in',
 ]
 
 
@@ -259,7 +259,9 @@ lexer = lex.lex(debug=False)
 
 
 precedence = (
-    ('right', 'ARROW'),
+    ('right', 'ARROW', ),
+    ('left', 'KEYWORD_LET', 'KEYWORD_LETREC'),
+    ('left', 'KEYWORD_IN', ),
 
     ('left', 'TICK_OPERATOR', ),
     ('left', 'OPERATOR', ),
@@ -443,18 +445,6 @@ def p_lambda_definition(prod):
     params = prod[2]
     assert params
     result = prod[4]
-    for varname in reversed(params):
-        result = Lambda(varname, result)
-    prod[0] = result
-
-
-def p_lambda_definition_keyword(prod):
-    '''expr : KEYWORD_LAMBDA SPACE parameters ARROW expr
-       expr : KEYWORD_LAMBDA SPACE parameters COLON expr
-    '''
-    params = prod[3]
-    assert params
-    result = prod[5]
     for varname in reversed(params):
         result = Lambda(varname, result)
     prod[0] = result
