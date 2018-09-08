7e9a23a7ae7566f82999c88973132677655dfe06
Manuel Vázquez Acosta <manuel@merchise.org>
2018-09-08 18:31:07 -0400

    Add simple program tests.

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -6,3 +6,41 @@
 #
 # This is free software; you can do what the LICENCE file allows you to.
 #
+from xopgi.ql.lang import parse
+
+
+def test_simple_one_definition():
+    assert parse('def id x = x')
+
+
+def test_simple_functions_definition():
+    assert parse('''
+       def id x = x
+
+       const :: a -> b -> a
+       def const a x = a
+
+    ''')
+
+
+def test_datatype_simple():
+    assert parse('data Then a = Then a')
+
+
+def test_datatype_simple2():
+    assert parse('''
+       data Then a = Then a
+       data Else a = Else a
+    ''')
+
+
+def test_if_program():
+    parse('''
+        data Then a = Then a
+        data Else a = Else a
+
+        if :: Bool -> Then a -> Else a -> a
+        def if True (Then x) _  = x
+            if False _ (Else x) = x
+
+    ''', debug=True)
--- xopgi/ql/lang/__init__.py
+++ xopgi/ql/lang/__init__.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # ---------------------------------------------------------------------
 # Copyright (c) Merchise Autrement [~º/~] and Contributors
@@ -6,3 +6,13 @@
 #
 # This is free software; you can do what the LICENCE file allows you to.
 #
+
+
+def parse(program_source: str, *, debug: bool = False):
+    '''Parse the program source and return its AST.
+
+    This function doesn't type-check the program.
+
+    '''
+    from .parsers import program_parser, lexer
+    return program_parser.parse(program_source, lexer=lexer, debug=debug)
