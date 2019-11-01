e7b41c6320abe9423e11838971288376a89ca073
Manuel Vázquez Acosta <manuel@merchise.org>
2019-11-01 09:57:57 -0400

    Advance the expression language using lark.
    I have just written the simplest of the expressions, not let expression, or
    data type, etc; just "arithmetic" expressions.
    
    Application has become a *list-like* element of tree because, otherwise I
    would get the wrong association, i.e: the tree for "a b c", would be
    equivalent to "a (b c)", and we expect "(a b) c".
    
    I'm using heavily the Lark feature of hiding concrete elements of the tree (by
    using "_" in rules).  Without it parsing "a f g" gets a tree like:
    
        >>> print(lark.parse(r"a f g").pretty())
        expr
          expr_term0
            expr_term1
              expr_term2
                expr_term6
                  expr_term7
                    expr_term9
                      expr_factor
                        application
                          expr_identifier	a
                          expr_identifier	f
                          expr_identifier	g
    
    And with it:
    
        >>> print(expr_lark.parse(r"a f g").pretty())
        expr
          application
            expr_identifier	a
            expr_identifier	f
            expr_identifier	g
    
    All expr_termN levels are hidden unless they actually add some value (the
    operator):
    
        >>> print(expr_lark.parse(r"a f >>= g").pretty())
        expr
          expr_term2
            application
              expr_identifier	a
              expr_identifier	f
            infixl_operator_2	>>=
            expr_identifier	g

--- xotl/fl/parsers/larkish.py
+++ xotl/fl/parsers/larkish.py
@@ -7,7 +7,20 @@
 # This is free software; you can do what the LICENCE file allows you to.
 #
 import os.path
-from lark import Lark, Transformer
+from typing import Iterable
+
+from lark import Lark, Transformer, Token
+from lark.indenter import Indenter
+
+
+class LexerHelper:
+    def __init__(self):
+        pass
+
+    def process(self, stream: Iterable[Token]) -> Iterable[Token]:
+        for token in stream:
+            yield token
+
 
 type_expr_parser = Lark.open(
     os.path.join(os.path.dirname(__file__), "grammar.lark"),
@@ -15,4 +28,13 @@ type_expr_parser = Lark.open(
     propagate_positions=True,
     start="type_expr",
     debug=True,
+    postlex=LexerHelper(),
+)
+expr_parser = Lark.open(
+    os.path.join(os.path.dirname(__file__), "grammar.lark"),
+    lexer="standard",
+    propagate_positions=True,
+    start="expr",
+    debug=True,
+    postlex=LexerHelper(),
 )
